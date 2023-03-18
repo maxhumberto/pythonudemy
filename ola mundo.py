12e4da8dcc9c56c8240171a3af7b695c45a562f5
Humberto Silva Lucas <maxhumberto@gmail.com>
2023-03-18 15:22:23 -0300

    exercicios python do curso de udemy

---
+++ ola mundo.py
@@ -0,0 +1,21 @@
+from numbers import Integral
+
+
+print ("Ola mundo!") 
+print(3+5)
+7+7
+
+#tipo numericos
+Integral
+test = 0b100
+print("100 Binary: ", test)
+test = 0o100
+print("100 Octal: ", test)
+test = 100
+print("100 Decimal: ", test)
+test = 0x100
+print("100 Hexadecimal: ", test)
+
+print(bin(test))
+print(oct(test))
+print(hex(test))
